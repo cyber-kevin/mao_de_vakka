668858e7edd03d254a53703dec598e5a6ffc1898
Kevin Ferreira <devkevferreira@gmail.com>
2022-11-30 18:09:18 -0300

    Criando tela inicial / Adicionando cor e imagem

---
+++ android/app/src/main/kotlin/com/example/mao_de_vakka/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mao_de_vakka
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      backgroundColor: Color.fromARGB(255, 59, 219, 118),
+      body: Center(
+          child: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.only(top: 10),
+            child: Image.asset(
+              'assets/images/vakka_logo.png',
+            ),
+          ),
+        ],
+      )),
+    ));
+  }
+}
